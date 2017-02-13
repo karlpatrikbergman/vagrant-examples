ed7166e11b7507c9fb1b8dfb1f12704b781de80c
Patrik Bergman <patrik.bergman@infinera.com>
2017-02-13 11:14:14 +0100

    japp

--- virtualbox/centos-kvm-qemu/scripts/bootstrap_ansible.sh
+++ virtualbox/centos-kvm-qemu/scripts/bootstrap_ansible.sh
@@ -1,11 +1,15 @@
-#!/bin/bash - 
+#!/bin/bash
 
 set -e
 
-#!/usr/bin/env bash
-
-sudo yum clean all
+echo "Installing Ansible..."
+sudo yum -y install epel-release
 sudo yum -y update
+sudo yum -y -d 10 install ansible
+sudo yum -y install wget
+
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+cp /vagrant/files/CentOS7-local.repo /etc/yum.repos.d/CentOS7-local.repo
 
 
 # For local ip network testing. Simple web page on port 80
